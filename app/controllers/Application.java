d6fde006dae00e14c309ee616781203920ed662a
Sheena Luu <sheena.luu@boldradius.com>
2015-09-09 16:19:57 -0400

    initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import models.User;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.libs.Json;
+
+import java.util.HashMap;
+
+public class Application extends Controller {
+
+    static HashMap<Long, User> map = new HashMap<Long,User>();
+    
+    public static Result index() {
+        return ok(views.html.index.render("Hello Play Framework"));
+    }
+
+    public static Result getUsers()
+    {
+        return ok(Json.toJson(map.values()));
+    }
+
+    public static Result getUser(Long id)
+    {
+        User user = map.get(id);
+        return user == null ? notFound() : ok(Json.toJson(user));
+    }
+
+    public static Result createUser(Long id, String name)
+    {
+        User newUser = new User(id, name);  //TODO Json.fromJson(request().body().asJson(), User.class);
+        map.put(id, newUser);
+        return created(Json.toJson(map.get(newUser.getId())));
+    }
+
+
+    public static Result updateUser(Long id, String name)
+    {
+        User user = new User(id, name);
+        User updated = map.replace(id, user);
+        return ok(Json.toJson(updated));
+    }
+
+
+    public static Result deleteUser(Long id) {
+        map.remove(id);
+        return noContent();
+    }
+}
---
+++ app/models/User.java
@@ -0,0 +1,20 @@
+package models;
+
+public class User{
+
+    private Long id;
+    private String name;
+
+    public User(Long id, String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
